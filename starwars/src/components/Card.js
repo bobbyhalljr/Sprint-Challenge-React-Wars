14c795af9e24b1a0e664b7cbe4a40a1f22d21a63
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-08-16 12:02:59 -0400

    I got the star wars characters on the screen

---
+++ starwars/src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const CharCard = styled.div`
+  width: 50%;
+  margin: 30px auto;
+  padding: 10px;
+  border-radius: 20px;
+  background: #f3f3f3;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 
+  0 6px 6px rgba(0, 0, 0, 0.23);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+function Card(props) {
+  return (
+    <>
+      <CharCard>
+        <h2>{props.name}</h2>
+        <p>Birth Year: {props.birthyear}</p>
+        <p>Height: {props.height}</p>
+        <p>Mass: {props.mass}</p>
+      </CharCard>
+    </>
+  );
+}
+
+export default Card;
